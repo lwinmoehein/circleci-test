6bbbadf53f771583b008f286fbf5073d008ae0f7
lwinmoehein <dev.lwinmoehein@gmail.com>
2022-09-28 10:24:50 +0630

    initial commit

---
+++ app/Http/Controllers/CircleCIController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\WebhookNotification;
+use Illuminate\Http\{JsonResponse, Response};
+
+class CircleCIController extends Controller
+{
+    //
+    public function getAllNotifications()
+    : JsonResponse {
+
+        return response()->json();
+    }
+
+    public function handleNotification()
+    : JsonResponse {
+
+        return response()
+            ->json(null, Response::HTTP_NO_CONTENT);
+    }
+
+}
